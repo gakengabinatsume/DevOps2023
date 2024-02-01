e23f0e978aef2e80e17f22a3096f21bd3c70120e
Gabriela Miron <141765846+gakengabinatsume@users.noreply.github.com>
2024-02-01 18:43:43 +0200

    Update PrintPDF.py

--- Final_project/Jenkins/PrintPDF.py
+++ Final_project/Jenkins/PrintPDF.py
@@ -3,7 +3,7 @@ import PyPDF2
 # Open the PDF file
 with open('CV.pdf', 'rb') as file:
     # Create a PDF reader object
-    pdf_reader = PyPDF2.PdfFileReader(file)
+    pdf_reader = PyPDF2.PdfFileReader(CV.pdf)
 
     # Get the total number of pages in the PDF
     num_pages = pdf_reader.numPages
