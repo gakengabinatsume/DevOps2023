c5ea71cd7e8231090a57a14642deb167d1a24ade
Gabriela Miron <141765846+gakengabinatsume@users.noreply.github.com>
2024-02-02 15:51:36 +0200

    Update PrintPDF.py

--- Final_project/Jenkins/PrintPDF.py
+++ Final_project/Jenkins/PrintPDF.py
@@ -1,16 +1,3 @@
-from pypdf import PdfReader
-
-reader = PdfReader("CV.pdf")
-
-# Print the number of pages in the PDF
-print(f"There are {len(reader.pages)} Pages")
-
-# Get the first page (index 0) 
-page = reader.pages[0]
-# Use extract_text() to get the text of the page
-print(page.extract_text())
-
-# Go through every page and get the text
-for i in range(len(reader.pages)):
-  page = reader.pages[i]
-  print(page.extract_text())
+import os
+path = 'CV.pdf'
+os.system(path)
